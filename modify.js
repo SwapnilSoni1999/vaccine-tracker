eb2658ba2826dddb10bd1eeaaa2e45e6bd503f71
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-03 15:51:13 +0530

    new script with nanoid

--- modify.js
+++ modify.js
@@ -1,20 +1,22 @@
 const fs = require('fs')
+const nanoid = require('nanoid').nanoid
 const users = JSON.parse(fs.readFileSync('db.json')).users
 
 const newData = {
     users: []
 }
 for (const user of users) {
-    const pincode = user.pincode
-    const age_group = user.age_group
-    if (pincode && age_group) {
-        const newUser = { ...user, tracking: [{ pincode, age_group }] }
-        delete newUser.pincode
-        delete newUser.age_group
+    const tracking = user.tracking
+    if (Array.isArray(tracking) && tracking.length) {
+        const newTracking = []
+        for (const t of tracking) {
+            if (!t.id) {
+                const nt = { ...t, id: nanoid() }
+                newTracking.push(nt)
+            }
+        }
+        const newUser = { ...user, tracking: newTracking }
         newData.users.push(newUser)
-    } else {
-        const newUser = { ...user, tracking: [] }
-        newData.users.push(newUser) 
     }
 }
 

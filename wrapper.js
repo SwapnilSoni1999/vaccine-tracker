ceb7e0332068abacb31a93e30b824d96f1e80f8a
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-07 22:54:51 +0530

    change headers

--- wrapper.js
+++ wrapper.js
@@ -5,19 +5,7 @@ const secretKey = "b5cab167-7977-4df1-8027-a63aa144f04e"
 const AES_KEY = "CoWIN@$#&*(!@%^&"
 
 const headers = {
-    'authority': 'cdn-api.co-vin.in',
-    'cache-control': 'max-age=0',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
-    'sec-ch-ua-mobile': '?0',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'sec-fetch-site': 'none',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-user': '?1',
-    'sec-fetch-dest': 'document',
-    'accept-language': 'en-US,en-IN;q=0.9,en;q=0.8',
-    'if-none-match': 'W/"14c7-xM9aYt9EVsqHGvfnQctm12OVYZE"',
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0',
 }
 
 const sha256 = (data) => {

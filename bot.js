fd00b02fe091393753687cd5bad5fb2d08c396d6
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-14 13:24:02 +0530

    its not always the 0th element

--- bot.js
+++ bot.js
@@ -686,7 +686,7 @@ bot.action(/remove-pin--.*/, async (ctx) => {
     try {
         const trackingId = ctx.update.callback_query.data.split('remove-pin--')[1]
         const { tracking } = await User.findOne({ $and: [ { chatId: ctx.update.callback_query.from.id }, { 'tracking._id': trackingId } ] }).select('tracking')
-        const { pincode, age_group } = tracking[0]
+        const { pincode, age_group } = tracking.find(t => t._id == trackingId)
         await User.updateOne({ chatId: ctx.update.callback_query.from.id }, { $pull: { tracking: { _id: trackingId } } })
         return await ctx.editMessageText(`Removed ${pincode}|${age_group} from your tracking list.`)
     } catch (err) {

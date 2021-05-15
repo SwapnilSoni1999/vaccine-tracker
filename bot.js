d01f762848951dc6e611b3787378444870536c45
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-16 02:47:58 +0530

    idk

--- bot.js
+++ bot.js
@@ -675,7 +675,7 @@ bot.command('beneficiaries', inviteMiddle, authMiddle, async (ctx) => {
         for (const txt of txts) {
             await ctx.reply(txt, { parse_mode: 'HTML' })
         }
-        const validBenef = ben.filter(b => (!!b.dose1_date || !!b.dose2_date))
+        const validBenef = ben.filter(b => ((b.dose1_date ? true:false) || (b.dose2_date ? true:false)))
         const markupButton = validBenef.map(b => ([{ text: b.name, callback_data: `benef--${b.beneficiary_reference_id}` }]))
         await ctx.reply('Please choose preferred beneficiary for auto booking.', { reply_markup: {
             inline_keyboard: markupButton

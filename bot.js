6444cae1cc2f3b09a874436dadacfd13a5fadb31
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-18 14:46:06 +0530

    avoid 1 slot available message

--- bot.js
+++ bot.js
@@ -998,7 +998,7 @@ async function trackAndInform() {
             console.log('Centers:', centers.length, 'District:', districtId)
             const available = centers.reduce((acc, center) => {
                 const tmpCenter = { ...center }
-                const sessions = center.sessions.filter(session => (session.available_capacity > 0) && (session.slots.length > 0))
+                const sessions = center.sessions.filter(session => (session.available_capacity > 1) && (session.slots.length > 0))
                 if (sessions.length) {
                     tmpCenter.sessions = sessions
                     acc.push(tmpCenter)

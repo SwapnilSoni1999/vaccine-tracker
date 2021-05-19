c34a17d00dc264008e1dc6a38f89dfa327a1447b
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-19 18:14:59 +0530

    [2/2] send appointment slip

--- wrapper.js
+++ wrapper.js
@@ -200,7 +200,7 @@ class CoWIN {
             },
             data: payload
         })
-        return res.data
+        return res.data.appointment_confirmation_no
     }
 
     static async getAppointmentSlip(appointmentId, token, chatId) {

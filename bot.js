a0e9fa1cec35eefd9586a805a67b17c71baf7938
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-05-19 21:37:37 +0530

    fucc govt

--- bot.js
+++ bot.js
@@ -529,7 +529,7 @@ const districtSelection = new Scenes.WizardScene(
     async (ctx) => {
         try {
             const state_nam = ctx.message.text
-            const { state_id, state_name } = ctx.wizard.state.states.find(s => s.state_name == state_nam)
+            const { state_id, state_name } = ctx.wizard.state.states.find(s => s.state_name.trim() == state_nam.trim())
             if (!state_id) {
                 await ctx.reply('Sorry invalid selection. Try again /district and Please choose valid state.', { reply_markup: { remove_keyboard: true } })
                 return ctx.scene.leave()
@@ -563,7 +563,7 @@ const districtSelection = new Scenes.WizardScene(
         try {
             const district_nam = ctx.message.text
             const districts = await CoWIN.getDistrict(ctx.wizard.state.state_id)
-            const { district_id, district_name } = districts.find(d => d.district_name == district_nam)
+            const { district_id, district_name } = districts.find(d => d.district_name.trim() == district_nam.trim())
             if (!district_id) {
                 await ctx.reply('Sorry invalid selection. Try again /district and Please choose valid district.', { reply_markup: { remove_keyboard: true } })
                 return ctx.scene.leave()

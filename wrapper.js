39636b06bfbc74256e13266a4fc6e0a3acbf8413
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-06-01 17:52:09 +0530

    wrapper: packetstream use indian ip only

--- wrapper.js
+++ wrapper.js
@@ -18,7 +18,7 @@ const headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0',
 }
 
-const packetStreamAgent = httpsOverHttp({ proxy: { host: 'proxy.packetstream.io', port: 31112, proxyAuth: 'houdini1:CbBxCbaJQgJURLKY' } })
+const packetStreamAgent = httpsOverHttp({ proxy: { host: 'proxy.packetstream.io', port: 31112, proxyAuth: 'houdini1:CbBxCbaJQgJURLKY_country-India' } })
 const PACKET_STREAM_COUNT = 25
 
 function getRandomAgent() {

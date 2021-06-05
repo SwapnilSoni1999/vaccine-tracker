2969b63943cbea8be5d99c7ff2161dccabf62826
SwapnilSoni1i999 <swapnil.soni12345@gmail.com>
2021-06-05 15:12:49 +0530

    fix pincode counting

--- bot.js
+++ bot.js
@@ -581,9 +581,10 @@ const sendToAll = new Scenes.WizardScene(
                 }
                 try {
                     if (user.allowed) {
-                        await bot.telegram.sendMessage(user.chatId, msg, { entities })
+                        const announcement = await bot.telegram.sendMessage(user.chatId, msg, { entities })
+                        await bot.telegram.pinChatMessage(user.chatId, announcement.message_id)
                         await sleep(200)
-                        startfrom }
+                    }
                 } catch (err) {
                     console.log("Broadcast error!", err)
                     if (err instanceof TelegramError) {
@@ -1356,7 +1357,13 @@ bot.command('sendall', async (ctx) => {
 bot.command('botstat', async (ctx) => {
     if (ctx.chat.id == SWAPNIL) {
         const users = await User.find({})
-        const txt = `Bot Stat!\n<b>Total Users</b>: ${users.length}\n<b>Verified Users (InviteKey)</b>: ${users.filter(u => u.allowed).length}\n<b>Unverified Users</b>: ${users.filter(u => !u.allowed).length}\n<b>Total pincodes in tracking</b>: ${users.filter(v => v.tracking).flat(1).length}\n<b>Logged in users</b>: ${users.filter(u => u.token).length}\n<b>Total Districts(Unique)</b>: ${[...new Set(users.filter(u => u.districtId).map(u => parseInt(u.districtId)))].length}\n<b>Total Districts</b>: ${users.filter(u => !!u.districtId).length}\n<b>Total users with AutoBook</b>: ${users.filter(u => u.autobook == true).length}`
+        const totalPincodes = users.reduce((count, user) => { 
+            if (user.tracking.length) {
+                count += user.tracking.length
+            }
+            return count
+        }, 0)
+        const txt = `Bot Stat!\n<b>Total Users</b>: ${users.length}\n<b>Verified Users (InviteKey)</b>: ${users.filter(u => u.allowed).length}\n<b>Unverified Users</b>: ${users.filter(u => !u.allowed).length}\n<b>Total pincodes in tracking</b>: ${totalPincodes}\n<b>Logged in users</b>: ${users.filter(u => u.token).length}\n<b>Total Districts(Unique)</b>: ${[...new Set(users.filter(u => u.districtId).map(u => parseInt(u.districtId)))].length}\n<b>Total Districts</b>: ${users.filter(u => !!u.districtId).length}\n<b>Total users with AutoBook</b>: ${users.filter(u => u.autobook == true).length}`
         return await ctx.reply(txt, { parse_mode: 'HTML' })
     }
 })

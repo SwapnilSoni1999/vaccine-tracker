bfacdd86e52190f3f9c6118695a47cdd042270c2
SwapnilSoni1i999 <swapnil.soni12345@gmail.com>
2021-06-08 15:14:33 +0530

    parse mode: html

--- bot.js
+++ bot.js
@@ -247,7 +247,7 @@ const inviteWizard = new Scenes.WizardScene(
                         await ctx.reply('Invitation accepted!')
                         const msg = `Hi, This bot can operate on selfregistration.cowin.gov.in.\nYou can send /help to know instructions about how to use this bot.\nDeveloped by <a href="https://github.com/SwapnilSoni1999">Swapnil Soni</a>`
                         await ctx.reply(msg, { parse_mode: 'HTML' })
-                        await ctx.reply(`Before you proceed further, Make sure you read the following notes:\n\n - <u>You must have atleast one beneficiary on your registered mobile number.</u>\n - <u>You must use login number which you used to register on cowin portal.`)
+                        await ctx.reply(`Before you proceed further, Make sure you read the following notes:\n\n - <u>You must have atleast one beneficiary on your registered mobile number.</u>\n - <u>You must use login number which you used to register on cowin portal.`, { parse_mode: 'HTML' })
                         return ctx.scene.leave()
                     } else {
                         await User.updateOne({ chatId: ctx.chat.id }, { allowed: false })

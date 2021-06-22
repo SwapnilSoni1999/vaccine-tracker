65f89cd7081d450c297d5c5a4211e28d712eb5e7
SwapnilSoni1i999 <swapnil.soni12345@gmail.com>
2021-06-22 16:09:09 +0530

    show total number of appointments done

--- bot.js
+++ bot.js
@@ -1494,8 +1494,19 @@ bot.command('botstat', async (ctx) => {
             }
             return count
         }, 0)
-        const totalSlips = fs.readdirSync('./appointments').length + 554
-        const txt = `Bot Stat!\n<b>Total Users</b>: ${users.length}\n<b>Verified Users (InviteKey)</b>: ${users.filter(u => u.allowed).length}\n<b>Unverified Users</b>: ${users.filter(u => !u.allowed).length}\n<b>Total pincodes in tracking</b>: ${totalPincodes}\n<b>Logged in users</b>: ${users.filter(u => u.token && u.allowed).length}\n<b>Total Districts(Unique)</b>: ${[...new Set(users.filter(u => u.districtId && u.allowed).map(u => parseInt(u.districtId)))].length}\n<b>Total Districts</b>: ${users.filter(u => !!u.districtId && u.allowed).length}\n<b>Total users with AutoBook</b>: ${users.filter(u => u.autobook == true && u.allowed).length}\n<b>Total users booked with autobook</b>: ${totalSlips}`
+        const totalSlips = (await User.aggregate([
+            {
+                $unwind: "$beneficiaries"
+            },
+            {
+                $project: {
+                    sizes: { $size: {
+                        $ifNull: ["$beneficiaries.appointments", []]
+                    } }
+                }
+            }
+        ])).reduce((total, v) => total += v.sizes, 0)
+        const txt = `Bot Stat!\n<b>Total Users</b>: ${users.length}\n<b>Verified Users (InviteKey)</b>: ${users.filter(u => u.allowed).length}\n<b>Unverified Users</b>: ${users.filter(u => !u.allowed).length}\n<b>Total pincodes in tracking</b>: ${totalPincodes}\n<b>Logged in users</b>: ${users.filter(u => u.token && u.allowed).length}\n<b>Total Districts(Unique)</b>: ${[...new Set(users.filter(u => u.districtId && u.allowed).map(u => parseInt(u.districtId)))].length}\n<b>Total Districts</b>: ${users.filter(u => !!u.districtId && u.allowed).length}\n<b>Total users with AutoBook</b>: ${users.filter(u => u.autobook == true && u.allowed).length}\n<b>Total Appointments</b>: ${totalSlips}`
         return await ctx.reply(txt, { parse_mode: 'HTML' })
     }
 })

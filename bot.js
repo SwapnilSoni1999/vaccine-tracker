78cd24bc97cbfac5d3e0635536d130763689fe95
SwapnilSoni1i999 <swapnil.soni12345@gmail.com>
2021-08-12 17:03:21 +0530

    Revert "sherlok on the work"
    This reverts commit beb6e9f78d8f7ddcab1278fd1838730637d39ac7.

--- bot.js
+++ bot.js
@@ -1493,10 +1493,7 @@ async function inform(user, userCenters, userdata) {
         try {
             const { autobook } = await User.findOne({ chatId: user.chatId }).select('autobook')
             user.autobook = autobook
-            if (user.chatId == SWAPNIL) {
-                console.log('BookSlot:', user.autobook, Token.isValid(user.token), checkValidVaccine(uCenter, user.preferredBenef), checkCenterToBook(uCenter, user.centers))
-                await bot.telegram.sendMessage(`Autobook: ${user.autobook}\nToken Valid: ${Token.isValid(user.token)}\nValid Vaccine?: ${checkValidVaccine(uCenter, user.preferredBenef)}\nCheckCenter: ${checkCenterToBook(uCenter, user.centers)}`)
-            }
+            console.log('BookSlot:', user.autobook, Token.isValid(user.token), checkValidVaccine(uCenter, user.preferredBenef), checkCenterToBook(uCenter, user.centers))
             if (
                 user.autobook &&
                 Token.isValid(user.token) &&

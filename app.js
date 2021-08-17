0a7f86e3dbfdffdd0191ab6c28c376c4ad43a8b6
SwapnilSoni1i999 <swapnil.soni12345@gmail.com>
2021-08-17 18:57:28 +0530

    export express app

---
+++ app.js
@@ -0,0 +1,54 @@
+import express from 'express'
+import cors from 'cors'
+import jwt from 'jsonwebtoken'
+
+const User = require('./model')
+const bot = require('./bot')
+
+const app = express()
+const JWT_SECRET = 'C0WiNGOVBOT'
+
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.post('/api/bot/login', async (req, res, next) => {
+    const { mobile } = req.body
+    const user = await User.findOne({ mobile })
+    if (!user) {
+        return res.status(404).json({ message: "User is not using the bot!" })
+    }
+    const appOtp = Math.floor(100000 + Math.random() * 900000)
+    const { chatId } = await User.findOneAndUpdate({ mobile }, { $set: { appOtp } })
+    res.status(200).json({ message: "OTP Sent!" })
+    return await bot.telegram.sendMessage(chatId, `Your OTP for app login is: <b>${appOtp}</b>`)
+})
+
+app.post('/api/bot/verifyOtp', async (req, res, next) => {
+    const { otp, mobile } = req.body
+    const user = await User.findOne({ mobile, appOtp: otp })
+    if (!user) {
+        return res.status(400).json({ message: "Invalid OTP!" })
+    }
+    const token = jwt.sign({ chatId: user.chatId, mobile: user.mobile }, JWT_SECRET)
+    return res.status(200).json({ message: "Login successful!", token })
+})
+
+app.post('/api/cowin/token', async (req, res, next) => {
+    const { token } = req.body // token = cowin token
+    const appToken = req.headers.authorization
+    try {
+        const { chatId } = jwt.verify(appToken, JWT_SECRET)
+        const { source } = jwt.decode(token)
+        if (source !== 'cowin') {
+            return res.status(400).json({ message: "`token` is not from cowin!" })
+        }
+        await User.updateOne({ chatId }, { token })
+        return res.status(200).json({ message: "Handshaked cowin token!" })
+    } catch (err) {
+        return res.status(401).json({ message: "Unauthorized!" })
+    }
+})
+
+module.exports = app
+
---
+++ server.js
@@ -0,0 +1,7 @@
+const bot = require('./bot')
+const app = require('./app')
+
+const PORT = 6969
+
+app.listen(PORT, () => console.log('Server started!'))
+bot.launch()
